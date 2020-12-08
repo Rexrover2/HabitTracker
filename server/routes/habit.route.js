344d48dc91a7f52b2641546f7a0442a19f5b11fa
Rexrover2 <lsleong@student.unimelb.edu.au>
2020-12-08 17:14:28 +1100

    Implemented routes for habit api
    get user's habits
    post new habit
    delete user's habit
    post entry for habit
    delete entry for habit
    post a new note for a habit
    delete a note for a habit
    get all comments for a habit

--- server/routes/habit.route.js
+++ server/routes/habit.route.js
@@ -1,25 +1,145 @@
 // Router for habits
 // Operators: CRUD (Create, Read, Update, Delete)
 
-// Create a new router
+const pool = require("../db");
 const router = require("express").Router();
 
-// GET current user - /
-router.get("/", (req, res) => {
-  res.set("Content-Type", "application/json");
-  res.send('{"message": "Your first habit request!"}');
+// Base = /habit
+// TODO: ADD status codes and constraints -> ie. var char lengths!
+
+
+// GET list of all habits - /:username
+router.get("/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const habits = await pool.query(
+      `SELECT * FROM "habit" \
+      WHERE username = '${username}';`
+    );
+    res.json(habits.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
-// PATCH modify user fields - /
+// POST create new habit - /:username
+router.post("/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { name, datestarted, dateended, streakgoal, iconno } = req.body;
+    // TODO: NOT SQL INJECTION SAFE
+    const habits = await pool.query(
+      `INSERT INTO "habit" (name, username, datestarted, dateended, streakgoal, iconno) \
+      VALUES ('${name}', '${username}', TO_DATE('${datestarted}', 'DD/MM/YYYY'), $1, $2, $3)`,
+      [dateended, streakgoal, iconno]
+    );
+    res.json(habits.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-// DELETE current user - /
+// TODO: PATCH modify habit information - /
 
-// POST sign up - /
+// DELETE specific user's habit - /
+router.delete("/", async (req, res) => {
+  try {
+    const { hid } = req.body;
+    const habits = await pool.query(
+      `DELETE FROM "habit" \
+      WHERE hid=$1`,
+      [hid]
+    );
+    res.json(habits.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-// POST log in - /
+// GET current steak in days - /
 
-// POST log out - /
+// GET longest steak in days - /
 
-// POST log out everywhere - /
+// POST create a new entry for a given habit - /entry/:hid
+router.post("/entry/:hid", async (req, res) => {
+  try {
+    const { hid } = req.params;
+    const { date } = req.body;
+    // TODO: if username, email != fall and are both strings else return 4**
+    const entry = await pool.query(
+      `INSERT INTO "entry" (hid, date) \
+      VALUES ($1, TO_DATE('${date}', 'DD/MM/YYYY'))`,
+      [hid]
+    );
+    res.json(entry.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// DELETE an entry for a given date - /entry
+router.delete("/entry", async (req, res) => {
+  try {
+    const { id } = req.body;
+    console.log(id)
+    const entry = await pool.query(
+      `DELETE FROM "entry" \
+      WHERE id=$1`,
+      [id]
+    );
+    res.json(entry.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// POST create a new note- /note/:hid
+router.post("/note/:hid", async (req, res) => {
+  try {
+    const { hid } = req.params;
+    const { note } = req.body;
+    // TODO: NOT SQL INJECTION SAFE
+    const notes = await pool.query(
+      `INSERT INTO "notes" (hid, note) \
+      VALUES ($1, '${note}')`,
+      [hid]
+    );
+    res.json(notes.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// DELETE a note - /note/:id
+router.delete("/note", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const notes = await pool.query(
+      `DELETE FROM "notes" \
+      WHERE id=$1`,
+      [id]
+    );
+    res.json(notes.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// PATCH edit existing comment for given time and date - /
+
+// GET Get all comment instances for a given habit - /note/:hid
+router.get("/note/:hid", async (req, res) => {
+  try {
+    const { hid } = req.params;
+    const notes = await pool.query(
+      `SELECT * FROM "notes" \
+      WHERE hid=($1)`,
+      [hid]
+    );
+    res.json(notes.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 module.exports = router;
