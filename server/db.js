3dff387727bc3fea522ec9d32999e6b4b428e018
Rexrover2 <lsleong@student.unimelb.edu.au>
2020-12-08 17:11:57 +1100

    Removed DB name from connection request

--- server/db.js
+++ server/db.js
@@ -8,7 +8,6 @@ const Pool = require('pg').Pool;
 // Connect to postgresql database hosted on aws rds. 
 const pool = new Pool({
   host     : process.env.RDS_HOSTNAME,
-  database : process.env.RDS_DB_NAME,
   user     : process.env.RDS_USERNAME,
   password : process.env.RDS_PASSWORD,
   port     : process.env.RDS_PORT
