0c833400f35db31ca89990d3c796df7a55f86fc2
Rexrover2 <lsleong@student.unimelb.edu.au>
2020-12-13 14:04:24 +1100

    Added routing to react app - User and Main Pages

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import logo from './logo.svg';
+import './App.css';
+
+const Main = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the main page!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const User = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the User page!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route path="/u/:userId" component={User} />
+        <Route path="*" component={Main} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
