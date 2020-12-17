0d162cbe8b19d7310fd3b5b7d72fb337f5da3075
Rexrover2 <lsleong@student.unimelb.edu.au>
2020-12-17 17:04:48 +1100

    Added Social Icons and Links for navbar buttons

--- client/src/Pages/Main.tsx
+++ client/src/Pages/Main.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React from 'react';
 import logo from '../logo.svg';
 import Navbar from '../Components/Navbar';
 
-
-
 const Main: React.FC<undefined> = () => {
   return (
-    <div className="App" style={{display:"flex", flexDirection:"column", minHeight:"100vh", overflowX:"hidden"}}>
-        <MainNavbar as="header" />
-        <div style={{ display:"flex", flexDirection:"column", minHeight:"100vh", alignItems:"center"}}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to the main page!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
-        <MainFooter as="footer" />
+    <div
+      className="App"
+      style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden' }}
+    >
+      <MainNavbar as="header" />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          alignItems: 'center',
+        }}
+      >
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the main page!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+      <MainFooter as="footer" />
     </div>
   );
 };
@@ -29,36 +37,43 @@ interface Props {
   as: string;
 }
 
-const MainNavbar: React.FC<Props> = (props: Props) => (
-  <Navbar {...props} secondary style={{display:"flex"}}>
-    <Navbar.Left style={{flex:1}}>
-      Camel Pages
-    </Navbar.Left>
+const MainNavbar: React.FC<Props> = (props: Props) => {
+  return (
+    <Navbar {...props} text style={{ margin: 0, display: 'flex' }}>
+      <Navbar.Left style={{ flex: 1 }}>
+        <Navbar.Link name="My Habits" to="/u/law" />
+      </Navbar.Left>
 
-    <Navbar.Center style={{flex:1}}>
-      Camel Pages
-    </Navbar.Center>
+      <Navbar.Center style={{ flex: 1 }}>
+        <Navbar.WebIcon/>
+      </Navbar.Center>
 
-    <Navbar.Right style={{flex:1}}>
-      <Navbar.ProfileIcon/>
-    </Navbar.Right>
-  </Navbar>
-)
+      <Navbar.Right style={{ flex: 1 }}>
+        <Navbar.ProfileIcon />
+      </Navbar.Right>
+    </Navbar>
+  );
+};
 
 const MainFooter: React.FC<Props> = (props: Props) => (
-  <Navbar {...props} attached="bottom" style={{display:"flex"}}>
-    <Navbar.Left style={{flex:1}}>
-      Camel Pages
+  <Navbar {...props} attached="bottom" style={{ display: 'flex' }}>
+    <Navbar.Left style={{ flex: 1 }}>
+      <Navbar.SocialIcon
+        color="linkedin"
+        icon="linkedin"
+        href="https://www.linkedin.com/in/lawrenceleong5/"
+      />
+      <Navbar.SocialIcon
+        color="github"
+        icon="github"
+        href="https://github.com/Rexrover2"
+      />
     </Navbar.Left>
 
-    <Navbar.Center style={{flex:1}}>
-      Camel Pages
-    </Navbar.Center>
+    <Navbar.Center style={{ flex: 1 }}></Navbar.Center>
 
-    <Navbar.Right style={{flex:1}}>
-      pew
-    </Navbar.Right>
+    <Navbar.Right style={{ flex: 1 }}></Navbar.Right>
   </Navbar>
-)
+);
 
 export default Main;
