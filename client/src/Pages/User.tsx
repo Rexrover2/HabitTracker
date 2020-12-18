e8f8cd72613840f3726615bd03e13a7ae4078b56
Lawrence Leong <lawrence.leong5@gmail.com>
2020-12-19 09:55:01 +1100

    Created a temporary clickable hexagon

--- client/src/Pages/User.tsx
+++ client/src/Pages/User.tsx
@@ -5,18 +5,30 @@ import Footer from './Footer';
 //style={{ stroke: '#ffe802' }} 
 const MyHexagon = () => {
   const [selected, setSelected] = useState<boolean>(false);
-  const handleClick = () => {};
+  const handleClick = () => {setSelected(!selected)};
+
+  const colourScheme = {
+    unselected: {
+      stroke: '#993300',
+      fill: '#808080',
+    },
+    selected: {
+      stroke: '#ffe802',
+      fill: '#ff8e02',
+    },
+  };
 
   return (
     <Hexagon
       flatTop={true}  
       style={{
         padding:"0.5em",
-        stroke: '#ffe802', 
-        stokeWidth:"30px",
-        minHeight:"300px",
-        fill:"#ff8e02"
+        ...(selected ? colourScheme.selected : colourScheme.unselected),
+        strokeWidth:20,
+        minHeight:"150px",
+        diagonal:50
       }}
+      onClick={handleClick}
     >
       <text x="50%" y="50%">1</text>
     </Hexagon>
