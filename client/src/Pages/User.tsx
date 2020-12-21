3078dbe17624f26dd80f92449963cdcb5c2f81d8
Lawrence Leong <lawrence.leong5@gmail.com>
2020-12-21 13:25:22 +1100

    Added Drop Down list for habit selection

--- client/src/Pages/User.tsx
+++ client/src/Pages/User.tsx
@@ -3,7 +3,14 @@ import MainNavbar from './Navbar';
 import Footer from './Footer';
 import HabitBoard from '../Components/HabitBoard';
 import HabitList from '../Components/HabitList';
-import { Header, List } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
+import Dropdown from '../Components/Dropdown';
+
+const data = [
+  { name: 'Full Stack Project', iconNo: 9 },
+  { name: 'Socialise', iconNo: 4 },
+  { name: 'Enough Sleep', iconNo: 13 },
+];
 
 const User: React.FC<undefined> = () => {
   return (
@@ -24,23 +31,26 @@ const User: React.FC<undefined> = () => {
           style={{
             display: 'flex',
             justifyContent: 'flexStart',
-            padding: '2em 2em 0em',
+            padding: '2em 2em 1em',
             width: '100%',
           }}
         >
-          <div
-            style={{
-              padding: '1em 2em 0.5em',
-              /* borderStyle: 'inset',
-              // borderColor: '#2d2d2d',
-              borderRadius: '25px', */
-            }}
-          >
+          <div>
             <Header as="h1">My Habits</Header>
-            <HabitList />
+            <HabitList data={data} />
           </div>
         </div>
-        <div style={{ margin: '2em' }}>
+        <div
+          style={{
+            display: 'flex',
+            padding: '2em',
+            justifyContent: 'flex-start',
+            width: '100%',
+          }}
+        >
+          <Dropdown data={data} />
+        </div>
+        <div style={{ margin: '0em 2em 2em' }}>
           <HabitBoard />
         </div>
       </div>
