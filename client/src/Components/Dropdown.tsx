cbd2c93fc4dddaf4f88ebfa0249a1ef7b13f976f
Lawrence Leong <lawrence.leong5@gmail.com>
2020-12-22 12:00:14 +1100

    made dropdown dynamic

--- client/src/Components/Dropdown.tsx
+++ client/src/Components/Dropdown.tsx
@@ -1,7 +1,8 @@
-import { Dropdown as SDropdown } from 'semantic-ui-react';
+import { Dropdown as SDropdown, DropdownProps } from 'semantic-ui-react';
 
 interface Props {
   data: Habit[];
+  setHabit: React.Dispatch<React.SetStateAction<string>>;
 }
 
 interface Habit {
@@ -10,6 +11,14 @@ interface Habit {
 }
 
 const Dropdown = (props: Props) => {
+  const onChange = (
+    event: React.SyntheticEvent<HTMLElement, Event>,
+    data: DropdownProps
+  ) => {
+    props.setHabit(data.value as string);
+    console.log(data.value);
+  };
+
   const habitOptions = props.data.map((inst, key) => ({
     key: key,
     text: inst.name,
@@ -17,14 +26,14 @@ const Dropdown = (props: Props) => {
   }));
 
   return (
-    // prettier-ignore
     <span>
-      Currently viewing entries for  {' '}
+      Currently viewing entries for{' '}
       <SDropdown
         inline
-        placeholder='Habit'
-        options={habitOptions}  
+        placeholder="Habit"
+        options={habitOptions}
         defaultValue={habitOptions[0].value}
+        onChange={onChange}
       />
     </span>
   );
