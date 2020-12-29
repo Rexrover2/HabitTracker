122507307e7274ee88ef559bcc06d537515b74fb
Lawrence Leong <lawrence.leong5@gmail.com>
2020-12-29 11:13:27 +1100

    Hacky Solution using setTimeout=1000

--- client/src/Pages/User.tsx
+++ client/src/Pages/User.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MainNavbar from './Navbar';
 import Footer from './Footer';
 import HabitBoard from '../Components/HabitBoard';
@@ -15,6 +15,7 @@ import { getAllByUser } from '../middleware/api';
 
 const User: React.FC<undefined> = () => {
   const user = 'lawrence';
+  const didMountRef = useRef(false);
   const [habitData, setHabitData] = useState<any>([]);
   const [entryData, setEntryData] = useState<any>([]);
   const [notesData, setNotesData] = useState<any>([]);
@@ -23,70 +24,80 @@ const User: React.FC<undefined> = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      getAllByUser(user)
-        .then((data) => {
-          setHabitData(data.habits);
-          setEntryData(data.entries);
-          setNotesData(data.notes);
-          setHabit(data.habits[0].name);
-        })
-        .then(() => {
-          /* 
-          console.log(entryData);
-          console.log(habitData); */
-          setIsFetching(false);
-        });
+      getAllByUser(user).then((data) => {
+        setHabitData(data.habits);
+        setEntryData(data.entries);
+        setNotesData(data.notes);
+        setHabit(data.habits[0].name);
+      });
     };
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const updateLoading = async () => {
+      setIsFetching(false);
+    };
+
+    setTimeout(() => {
+      if (entryData.length !== 0) {
+        updateLoading();
+        console.log(entryData);
+      }
+    }, 1000);
+  }, [entryData]);
 
   return (
     <div
       className="App"
       style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden' }}
     >
-      <MainNavbar as="header" page="user" />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-          alignItems: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'flexStart',
-            padding: '2em 2em 1em',
-            width: '100%',
-          }}
-        >
-          <div>
-            <Header as="h1">My Habits</Header>
-            <HabitList data={habitData} />
+      {isFetching ? null : (
+        <div>
+          <MainNavbar as="header" page="user" />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+              alignItems: 'center',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flexStart',
+                padding: '2em 2em 1em',
+                width: '100%',
+              }}
+            >
+              <div>
+                <Header as="h1">My Habits</Header>
+                <HabitList data={habitData} />
+              </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                padding: '2em',
+                justifyContent: 'flex-start',
+                width: '100%',
+              }}
+            >
+              <Dropdown data={habitData} habit={habit} setHabit={setHabit} />
+            </div>
+            <div style={{ margin: '0em 2em 2em' }}>
+              <HabitBoard
+                isFetching={isFetching}
+                habit={habit}
+                entryData={entryData}
+                habitData={habitData}
+              />
+            </div>
           </div>
+          <Footer as="footer" />
         </div>
-        <div
-          style={{
-            display: 'flex',
-            padding: '2em',
-            justifyContent: 'flex-start',
-            width: '100%',
-          }}
-        >
-          <Dropdown data={habitData} habit={habit} setHabit={setHabit} />
-        </div>
-        <div style={{ margin: '0em 2em 2em' }}>
-          <HabitBoard
-            isFetching={isFetching}
-            habit={habit}
-            entryData={entryData}
-            habitData={habitData}
-          />
-        </div>
-      </div>
-      <Footer as="footer" />
+      )}
     </div>
   );
 };
