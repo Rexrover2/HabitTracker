9cf0704951f5484c7fdbadba4843e8f15c4373cc
Lawrence Leong <lawrence.leong5@gmail.com>
2021-01-07 12:26:54 +1100

    Added signup, login, logout placeholder controls for routes

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -15,3 +15,31 @@ router.post(url, async (req, res) => {
   authToken = parsedJson(res.body).access_token;
   console.log(authToken);
 });
+
+const signUp = async (req, res) => {
+  // TODO: Navigate to habit page
+};
+
+const logIn = async (req, res) => {
+  // TODO: Navigate to habit page
+};
+
+const logOut = async (req, res) => {
+  // TODO: Navigate to home page
+};
+
+/* const logOutAll = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const habits = await pool.query(
+      `SELECT * FROM "habit" \
+      WHERE username = $1;`,
+      [username]
+    );
+    res.json(habits.rows);
+  } catch {
+    res.sendStatus(400);
+  }
+}; */
+
+module.exports = { signUp, logIn, logOut };
