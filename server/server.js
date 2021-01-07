a2d711195d225b529a7e7998ec01f7a36607d6cf
Lawrence Leong <lawrence.leong5@gmail.com>
2021-01-07 17:57:19 +1100

    Attempt at creating cookie for frontend
    Experiencing ForbiddenError: invalid csrf token

--- server/server.js
+++ server/server.js
@@ -2,32 +2,76 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
+const bodyParser = require('body-parser');
+const serviceAccount = require('./serviceAccountKey.json');
+const admin = require('firebase-admin');
 
-const csrfMiddleware = csrf({ cookie: true });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const PORT = 5000;
 
 const app = express();
 
 // Middleware - cors
+
 app.use(cors());
-app.use(express.json());
+
 app.use(cookieParser());
+app.use(bodyParser.json());
+const csrfMiddleware = csrf({ cookie: true });
 app.use(csrfMiddleware);
 
-/** Tutorial on Auth0 api protection - https://scotch.io/tutorials/building-and-securing-a-modern-backend-api */
-// If we do not get the correct credentials, we’ll return an appropriate message
-/* app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).json({ message: 'Missing or invalid token' });
-  }
+app.all('*', (req, res, next) => {
+  console.log('Cookies');
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  next();
+});
+/* app.use(function (req, res, next) {
+  // res.locals._csrf = req.csrfToken();
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  next();
 }); */
-
-/** End of tutorial */
 
 // All routes are defined in ./routes
 apiRouter = require('./routes/routes');
 app.use('/api', apiRouter);
+
+app.get('/sessionLogout', (req, res) => {
+  res.clearCookie('session');
+  // res.redirect('/login');
+  res.end();
+});
+
+app.post('/sessionLogin', (req, res) => {
+  try {
+    const idToken = req.body.idToken.toString();
+    console.log('HI');
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+    admin
+      .auth()
+      .createSessionCookie(idToken, { expiresIn })
+      .then(
+        (sessionCookie) => {
+          const options = {
+            maxAge: expiresIn,
+            httpOnly: true,
+            path: 'http://localhost:3000',
+          };
+          res.cookie('session', sessionCookie, options);
+          res.end(JSON.stringify({ status: 'success' }));
+        },
+        (error) => {
+          res.status(401).send('UNAUTHORIZED REQUEST!');
+        }
+      );
+  } catch (err) {
+    console.log('HI');
+    console.error(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
