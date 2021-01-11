70d2803b616c23adf9f324380952dc0d8010d339
Lawrence Leong <lawrence.leong5@gmail.com>
2021-01-11 22:27:20 +1100

    I seriously purged auth0 from my codebase

--- environment.js
+++ environment.js
@@ -9,10 +9,4 @@ module.exports = {
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   port: process.env.RDS_PORT,
-  auth0Domain: process.env.AUTH0_DOMAIN,
-  auth0ApiIdentifier: process.env.AUTH0_AUDIENCE,
-  domain: process.env.REACT_APP_AUTH0_DOMAIN,
-  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
-  clientSecret: process.env.REACT_APP_AUTH0_CLIENT_SECRET,
-  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
 };
