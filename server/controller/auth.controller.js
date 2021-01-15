47907fbb1df1f81e1674bc8e506b8290fd6b3aac
Lawrence Leong <lawrence.leong5@gmail.com>
2021-01-15 11:37:34 +1100

    Remove redundant routes and controllers

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,25 +1,7 @@
-const config = require('../../environment');
-const router = require('express').Router();
 const pool = require('../db');
 
-const requestToken = {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${config.clientSecret}`,
-  },
-  body: `{"client_id":${config.clientId},"client_secret":${config.clientSecret},"audience":${config.audience},"grant_type":"client_credentials"}`,
-};
-
-let url = `https://${config.domain}/oauth/token`;
-
-router.post(url, async (req, res) => {
-  authToken = parsedJson(res.body).access_token;
-  console.log(authToken);
-});
-
-const signUp = async (req, res, next) => {
+const signUp = async (req, res) => {
   try {
-    // Email & password??
     const { uid } = req;
     const { username } = req.body;
     console.log(`Signing up Sir/Madam ${username}!`);
@@ -30,27 +12,11 @@ const signUp = async (req, res, next) => {
     );
 
     res.sendStatus(200);
-    // TODO: Navigate to habit page
   } catch (e) {
     return res.status(401).send({
       error: 'Something went wrong',
     });
   }
-};
-
-const logIn = async (req, res) => {
-  // Generate token
-  console.log('user controller, login called!');
-  // TODO: Navigate to habit page
-};
-
-const logOut = async (req, res) => {
-  // TODO: Navigate to home page
-};
-
-const secret = async (req, res) => {
-  console.log('user controller, secret called!');
-  // TODO: Navigate to habit page
 };
 
 /* const logOutAll = async (req, res) => {
@@ -67,4 +33,4 @@ const secret = async (req, res) => {
   }
 }; */
 
-module.exports = { signUp, logIn, logOut, secret };
+module.exports = { signUp };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,10 +4,4 @@ const authController = require('../controller/auth.controller');
 // Sign up
 router.post('/signup', authController.signUp);
 
-// Sign in
-router.post('/login', authController.logIn);
-
-// Test get secret resource
-router.get('/secret', authController.secret);
-
 module.exports = router;
