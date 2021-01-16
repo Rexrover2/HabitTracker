10bb4a4d84613061fcefed11632ac7b8a9dc6810
Lawrence Leong <lawrence.leong5@gmail.com>
2021-01-16 13:26:17 +1100

    Remove /server as it was replaced with /functions

--- server/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controller/auth.controller');
-
-// Sign up
-router.post('/signup', authController.signUp);
-
-module.exports = router;
